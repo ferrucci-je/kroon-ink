f3ef5c2f413ddafc8b31f1e06b1429ee6cc6d4e6
ferrucci-je <ferrucci.je@gmail.com>
2020-10-30 00:31:17 -0300

    first commit

---
+++ js/config.js
@@ -0,0 +1,30 @@
+// Configuration Object
+var config = {};
+
+/*-------------------------------------------------------------------*/
+/*      1. Form Messages
+/*-------------------------------------------------------------------*/
+
+config.formMessages = {
+    sent          : 'El mensaje fue enviado correctamente. Muchas Gracias!',
+    fail          : 'Ups! Surgió un error. Por favor intenta de nuevo',
+    failEmail     : 'E-mail incorrecto!',
+    emptyFields   : 'Completa todas las casillas requeridas!'
+};
+
+/*-------------------------------------------------------------------*/
+/*      2. Google Map
+/*-------------------------------------------------------------------*/
+
+config.gmap = {
+    latitude    : '',
+    longitude   : '',
+    zoom        : 15,
+    marker      : 'img/marker.png'
+};
+
+/*-------------------------------------------------------------------*/
+/*      3. Easy Background System Settings
+/*-------------------------------------------------------------------*/
+
+config.backgrouns = {};
